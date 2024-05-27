abd547f74ec1fb661fed02e7c28899bee0f98e17
kongkrog <125655140+kongkrog@users.noreply.github.com>
2024-05-27 08:30:14 +0700

    Add files via upload

--- dialogue_beta.js
+++ dialogue_beta.js
@@ -12,7 +12,6 @@ let defaultSpeed = 20;
 
 function updateProfileImage() {
     const fileInput = document.getElementById('fileInput');
-    const imageUrlText = document.getElementById('imageUrl');
     const profileImageDiv = document.getElementById('dialogueImage');
 
     // Check if a file is selected
@@ -23,11 +22,7 @@ function updateProfileImage() {
         if (file.type.startsWith('image/')) {
             const imageUrl = URL.createObjectURL(file);
             profileImageDiv.src = imageUrl;
-        } else {
-            imageUrlText.textContent = 'Selected file is not an image.';
         }
-    } else {
-        imageUrlText.textContent = 'No file selected.';
     }
 }
 
@@ -37,8 +32,6 @@ function clearImage() {
     const imageElement = document.getElementById('dialogueImage');
     if (imageElement) {
         imageElement.src = '';
-        document.getElementById('dialogueImage').style.display = 'none';
-        document.getElementById('dialogueText').style.paddingLeft = '0px';
     }
 }
 
@@ -69,24 +62,11 @@ function updateProfile() {
             break;
     }
 
-    originalText = dialogueText;
-
-    if (dialogueName == "") {
-        document.getElementById('extraDialogueUI').style.display = 'none';
-        document.getElementById('dialogueName').innerText = dialogueName;
-    } else {
-        document.getElementById('extraDialogueUI').style.display = 'flex';
-        document.getElementById('dialogueName').innerText = dialogueName;
-    }
+    textString = dialogueText;
+    document.getElementById('dName').innerText = dialogueName;
 
     if (fileInput.files.length > 0) {
-        document.getElementById('extraDialogueUI').style.display = 'none';
-        document.getElementById('dialogueName').value = '';
-        document.getElementById('dialogueImage').style.display = 'block';
-        document.getElementById('dialogueText').style.paddingLeft = '234px';
-    } else {
-        document.getElementById('dialogueImage').style.display = 'none';
-        document.getElementById('dialogueText').style.paddingLeft = '0px';
+        document.getElementById('dName').innerText = '';
     }
 }
 
@@ -94,6 +74,7 @@ document.getElementById("saveButton").addEventListener("click", function(event) 
     event.preventDefault(); // Prevent default form submission
     updateProfile(); // Call the updateProfile function
     updateProfileImage();
+    typewriterAnimation(textString)
 });
 
 document.getElementById("clearButton").addEventListener("click", function(event) {
@@ -139,6 +120,8 @@ function parseText(text) {
 function typewriterAnimation(textString) {
     const characters = [];
     const myCanvas = document.getElementById("dialogueCanvas");
+    const dialogueImage = document.getElementById("dialogueImage");
+    const dName = document.getElementById("dName").innerText;
     const ctx = myCanvas.getContext("2d");
 
     canvasWidth = myCanvas.scrollWidth;
@@ -203,16 +186,62 @@ function typewriterAnimation(textString) {
         ctx.fillRect(x, y+4, 4, lineHeight-8)
     }
 
-    drawCorner(1, 1);
-    drawCorner(canvasWidth-7, 1);
-    drawCorner(canvasWidth-7, canvasHeight-7);
-    drawCorner(1, canvasHeight-7);
+    function drawArrow(x, y, opacity) {
+        ctx.globalAlpha = opacity;
+        ctx.fillStyle = 'gray';
+        ctx.fillRect(x, y, 2, 20);
 
-    drawLineHorizontal(8, 2, canvasWidth-16);
-    drawLineHorizontal(8, canvasHeight-6, canvasWidth-16);
+        ctx.fillStyle = 'lightgray';
+        ctx.fillRect(x + 2, y + 2, 2, 16);
+        ctx.fillRect(x + 4, y + 4, 2, 12);
+        ctx.fillRect(x + 6, y + 6, 2, 8);
+        ctx.fillRect(x + 8, y + 8, 2, 4);
 
-    drawLineVertical(2, 8, canvasHeight-16)
-    drawLineVertical(canvasWidth-6, 8, canvasHeight-16);
+        ctx.fillStyle = 'white';
+        ctx.fillRect(x, y + 2, 2, 16);
+        ctx.fillRect(x + 2, y + 4, 2, 12);
+        ctx.fillRect(x + 4, y + 6, 2, 8);
+        ctx.fillRect(x + 6, y + 8, 2, 4);
+        ctx.globalAlpha = 1.0; // Reset opacity
+    }
+
+    function drawNameBox(nameText) {
+        ctx.font = '23px VCR_OSD_MONO';
+        ctx.fillStyle = 'gray';
+        ctx.fillText(nameText, 25, 110);
+
+        const charWidth = ctx.measureText(nameText).width;
+
+        ctx.fillStyle = 'white';
+        drawCorner(1, 66);
+        drawCorner(44 + charWidth, 66);
+        drawCorner(44 + charWidth, myCanvas.height - 187);
+        drawCorner(1, myCanvas.height - 187);
+    
+        drawLineHorizontal(8, 67, 43 + charWidth - 8);
+        drawLineHorizontal(8, myCanvas.height - 186, 43 + charWidth - 8);
+    
+        drawLineVertical(2, 73, myCanvas.height - 261);
+        drawLineVertical(44 + charWidth + 1, 73, myCanvas.height - 261);
+    }
+    
+    if (dName != '') {
+        drawNameBox(dName);
+    }
+    
+    drawCorner(1, 137);
+    drawCorner(myCanvas.width - 7, 137);
+    drawCorner(myCanvas.width - 7, myCanvas.height - 7);
+    drawCorner(1, myCanvas.height - 7);
+
+    drawLineHorizontal(8, 138, myCanvas.width - 16);
+    drawLineHorizontal(8, myCanvas.height - 6, myCanvas.width - 16);
+
+    drawLineVertical(2, 144, myCanvas.height - 152);
+    drawLineVertical(myCanvas.width - 6, 144, myCanvas.height - 152);
+
+    scale = 317 / dialogueImage.height
+    ctx.drawImage(dialogueImage, dialogueImage.width - (dialogueImage.width * scale), 0, dialogueImage.width * scale , dialogueImage.height, 0, 0, 263, 317);
 
     ctx.fillStyle = 'white';
     characters.forEach(({ char, xOffset, yOffset, font }) => {
@@ -225,18 +254,26 @@ function typewriterAnimation(textString) {
         return acc;
     }, []);
 
-
-    function drawTextWithWrapping(ctx, segments, canvasWidth, lineHeight) {
-        let xOffset = 21;
-        let yOffset = 41;
+    function drawTextWithWrapping(ctx, segments, globalxOffset, globalyOffset, canvasWidth, lineHeight) {
+        let xOffset = globalxOffset;
+        let yOffset = globalyOffset;
         let currentSegmentIndex = 0;
         let currentTextIndex = 0;
-        let currentSpeed = 10;
+        let currentSpeed = 25;
         let maxFontSize = 23;
         let isFirstLine = true;
+        let nextWordWidth = 0;
+        let pauseDuration = null;
+        let pauseTimeout = null;
 
         function drawNextCharacter() {
+
             if (currentSegmentIndex >= segments.length) {
+                pauseDuration = 3000; // Set 3 seconds pause at the end
+                playArrowAnimation();
+                setTimeout(() => {
+                    playArrowAnimation(false); // Hide arrow after 3 seconds
+                }, pauseDuration);
                 return;
             }
 
@@ -245,9 +282,14 @@ function typewriterAnimation(textString) {
             const char = text[currentTextIndex];
 
             if (segment.pause !== null) {
-                setTimeout(drawNextCharacter, segment.pause);
-                currentSegmentIndex++;
-                currentTextIndex = 0;
+                pauseDuration = segment.pause;
+                playArrowAnimation(true); // Show arrow during pause
+                pauseTimeout = setTimeout(() => {
+                    playArrowAnimation(false); // Hide arrow after pause
+                    currentSegmentIndex++;
+                    currentTextIndex = 0;
+                    drawNextCharacter();
+                }, pauseDuration);
                 return;
             }
 
@@ -259,12 +301,22 @@ function typewriterAnimation(textString) {
             }
 
             const charWidth = ctx.measureText(char).width;
-
-            if (xOffset + charWidth > canvasWidth && char.trim() !== '') {
-                xOffset = 21;
+            
+            if (char === ' ' && nextWordWidth > 0 && xOffset + nextWordWidth > canvasWidth) {
+                xOffset = globalxOffset;
                 yOffset += maxFontSize + lineHeight;
                 maxFontSize = 23;
                 isFirstLine = false;
+                nextWordWidth = 0;
+            } else if (char === ' ') {
+                const nextWord = text.substring(currentTextIndex + 1).split(' ')[0];
+                nextWordWidth = ctx.measureText(nextWord).width;
+            }
+
+            if (xOffset === globalxOffset && char === ' ') {
+                currentTextIndex++;
+                drawNextCharacter();
+                return;
             }
 
             if (yOffset === 0) {
@@ -278,7 +330,7 @@ function typewriterAnimation(textString) {
             }
 
             // Store character and position
-            characters.push({ char, xOffset, yOffset, font: ctx.font, wave: segment.wave, shake: segment.shake });
+            characters.push({ char, xOffset, yOffset, font: ctx.font, wave: segment.wave, shake: segment.shake});
 
             xOffset += charWidth;
             currentTextIndex++;
@@ -295,24 +347,52 @@ function typewriterAnimation(textString) {
         drawNextCharacter();
     }
 
+    let arrowVisible = false;
+    let arrowOpacity = 0;
+    let fadingIn = false;
+    let fadingOut = false;
+    const fadeSpeed = 0.05;
+    let arrowXOffset = 0;
+    let arrowDirection = 1;
+    let arrowCounter = 0;
+    const arrowUpdateInterval = 10;
+
+    function playArrowAnimation(showArrow = true) {
+        if (showArrow) {
+            arrowVisible = true;
+            fadingIn = true;
+            fadingOut = false;
+        } else {
+            fadingOut = true;
+            fadingIn = false;
+        }
+    }
+
     function animateCharacters() {
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, myCanvas.width, myCanvas.height);
+        
+        if (dName != '') {
+            drawNameBox(dName);
+        }
 
-        drawCorner(1, 1);
-        drawCorner(myCanvas.width - 7, 1);
+        drawCorner(1, 137);
+        drawCorner(myCanvas.width - 7, 137);
         drawCorner(myCanvas.width - 7, myCanvas.height - 7);
         drawCorner(1, myCanvas.height - 7);
 
-        drawLineHorizontal(8, 2, myCanvas.width - 16);
+        drawLineHorizontal(8, 138, myCanvas.width - 16);
         drawLineHorizontal(8, myCanvas.height - 6, myCanvas.width - 16);
 
-        drawLineVertical(2, 8, myCanvas.height - 16);
-        drawLineVertical(myCanvas.width - 6, 8, myCanvas.height - 16);
+        drawLineVertical(2, 144, myCanvas.height - 152);
+        drawLineVertical(myCanvas.width - 6, 144, myCanvas.height - 152);
+
+        scale = 317 / dialogueImage.height
+        ctx.drawImage(dialogueImage, dialogueImage.width - (dialogueImage.width * scale), 0, dialogueImage.width * scale , dialogueImage.height, 0, 0, 263, 317);
 
         ctx.fillStyle = 'white';
         const time = Date.now();
-        characters.forEach(({ char, xOffset, yOffset, font, wave, shake }) => {
+        characters.forEach(({ char, xOffset, yOffset, font, wave, shake}) => {
             ctx.font = font;
 
             if (wave) {
@@ -330,6 +410,41 @@ function typewriterAnimation(textString) {
             ctx.fillText(char, xOffset, yOffset);
         });
 
+        if (arrowVisible) {
+            if (fadingIn) {
+                arrowOpacity += fadeSpeed;
+                if (arrowOpacity >= 1) {
+                    arrowOpacity = 1;
+                    fadingIn = false;
+                }
+            } else if (fadingOut) {
+                arrowOpacity -= fadeSpeed;
+                if (arrowOpacity <= 0) {
+                    arrowOpacity = 0;
+                    arrowVisible = false;
+                    fadingOut = false;
+                }
+            }
+
+            arrowCounter++;
+            if (arrowCounter >= arrowUpdateInterval) {
+                if (arrowDirection === 1) {
+                    arrowXOffset += 1;
+                    if (arrowXOffset >= 2) {
+                        arrowDirection = -1;
+                    }
+                } else {
+                    arrowXOffset -= 1;
+                    if (arrowXOffset <= -2) {
+                        arrowDirection = 1;
+                    }
+                }
+                arrowCounter = 0;
+            }
+
+            const arrowX = canvasWidth - 33 + arrowXOffset;
+            drawArrow(arrowX, canvasHeight - 37, arrowOpacity);
+        }
         gif.addFrame(ctx, { copy: true, delay: 20 });
         requestAnimationFrame(animateCharacters);
     }
@@ -342,11 +457,13 @@ function typewriterAnimation(textString) {
     });
 
     gif.on('finished', function(blob) {
-        // Create an <img> element
         const gifImage = document.createElement('img');
-        gifImage.src = URL.createObjectURL(blob);
+        const gifResult = document.getElementById('gifResult');
 
-        // Get the download button
+        gifImage.src = URL.createObjectURL(blob);
+        gifResult.appendChild(gifImage);
+        downloadButton.style.display = 'inline-block';
+
         const downloadButton = document.getElementById('downloadButton');
         downloadButton.addEventListener('click', function() {
             const downloadLink = document.createElement('a');
@@ -358,13 +475,15 @@ function typewriterAnimation(textString) {
         });
     });
 
-    drawTextWithWrapping(ctx, parseText(textString), canvasWidth - 19, 12);
-    animateCharacters();
+    if (dialogueImage.getAttribute('src') != '') {
+        drawTextWithWrapping(ctx, parseText(textString), 21+230, 41+137, canvasWidth - 19, 10);
+        animateCharacters();
+    } else {
+        drawTextWithWrapping(ctx, parseText(textString), 21, 41+137, canvasWidth - 19, 10);
+        animateCharacters();
+    }
 
-    setTimeout(() => {
-        gif.render();
-    }, 5000);
+    // setTimeout(() => {
+    //     gif.render();
+    // }, 5000);
 }
-
-textString = "I-I think I might write a book about this![PS=400] People need to know!!! [SW]SINE WAVE. SINE WAVE.[/SW][SHAKE]SHAKING SHAKING SHAKING SHAKING SHAKING SHAKING.[/SHAKE]"
-typewriterAnimation(textString)
